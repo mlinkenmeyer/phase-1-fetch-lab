c250c43cafc7fdd9f64af5c015fde07586649a5d
Madison <mlinkenmeyer@gmail.com>
2023-08-06 22:34:54 -0500

    fetch lab complete

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (arrayOfTenBooks) {
+      renderBooks(arrayOfTenBooks);
+    });
   // To pass the tests, don't forget to return your fetch!
-  
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
